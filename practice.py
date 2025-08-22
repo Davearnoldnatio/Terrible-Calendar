51f931c38f9423c13457e2aef0bac795b00e9f1c
Davearnoldnatio <davearnoldnation@gmail.com>
2025-08-21 23:46:35 -0500

    started a practice file to practice using tkinter

---
+++ practice.py
@@ -0,0 +1,20 @@
+import tkinter as tk
+
+root = tk.Tk()  #created the window that the GUI is displayed on
+
+greetings = tk.Label(root, text = "hi") #sets greeting to a label that says hi
+greetings.pack() #puts the label on the window
+
+
+
+
+
+
+
+
+
+
+
+
+
+root.mainloop()
